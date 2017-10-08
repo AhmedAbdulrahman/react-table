bcb718b6d94b056926db98986d08efe8d970d3c6
Ahmed Abdulrahman <a.kasapbashi@live.com>
2017-10-08 22:56:17 +0200

    limit user data and remove noinfo

--- src/Helper.js
+++ src/Helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const fetchData =  (len) => {
-	const userURL = `https://randomuser.me/api/?results=${ len }`;
+	const userURL = `https://randomuser.me/api/?results=${ len }&inc=name,gender,location,cell,dob,email,phone&noinfo`;
 
     return axios.get(userURL)
     .then((response) => {
